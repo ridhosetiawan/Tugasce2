82a9dc422a642438ab0bed4e82bb099c38b0c374
ahmadridhosetiawan <32403430+ahmadridhosetiawan@users.noreply.github.com>
2017-10-22 01:16:46 +0800

    Mengitung dan juga menampilkan biaya parket (Rp)

--- Untitled5.cpp
+++ Untitled5.cpp
@@ -11,6 +11,8 @@ int main(){
 	double jumlahBoxP,biayaP,biayaPG,totalB;
 	jumlahBoxP = luasRumah/luas1Box;
 	cout<< "Jumlah box parket        :"<<jumlahBoxP<<endl;
+	biayaP = jumlahBoxP * 500;
+	cout << "Biaya Parket (Rp)       :"<<biayaP<<endl;
 
 	
 }
